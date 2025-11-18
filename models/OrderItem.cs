dc28742b1aa2e11c2f2fc1e4b6be11459a3b948b
adil19071 <blackheart12200@gmail.com>
2025-11-18 15:23:42 +0530

    Update OrderItem.cs

--- models/OrderItem.cs
+++ models/OrderItem.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace OnlineFoodOrderingSystem.Models
 {
     public class OrderItem
     {
         public int Id { get; set; }
+
         public int OrderId { get; set; }
         public Order? Order { get; set; }
 
