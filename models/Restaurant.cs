ae4edc41dc8911685e00d28982f2289d734e6c15
adil19071 <blackheart12200@gmail.com>
2025-11-18 15:33:29 +0530

    Update Restaurant.cs

--- models/Restaurant.cs
+++ models/Restaurant.cs
@@ -1,12 +1,10 @@
+using System;
+using System.Collections.Generic;
+
 namespace OnlineFoodOrderingSystem.Models
 {
     public class Restaurant
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Address { get; set; } = "";
-        public string Phone { get; set; } = "";
-        public string Description { get; set; } = "";
-        public ICollection<FoodItem>? FoodItems { get; set; }
+        // your existing properties here
     }
 }
