58a73d5d2cac1bdd2941d335cd061b00e9ee4ed5
adil19071 <blackheart12200@gmail.com>
2025-11-18 15:19:07 +0530

    Update Order.cs

--- models/Order.cs
+++ models/Order.cs
@@ -6,7 +6,7 @@ namespace OnlineFoodOrderingSystem.Models
     public class Order
     {
         public int Id { get; set; }
-        public DateTime OrderDate { get; set; }  // <-- THIS CAUSED ERROR
+        public DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
 
         public ICollection<OrderItem> Items { get; set; }
