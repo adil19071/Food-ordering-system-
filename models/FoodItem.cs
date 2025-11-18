6e4829a1d40c1713592ab1f85a1992f14c9ff17f
adil19071 <blackheart12200@gmail.com>
2025-11-18 15:27:49 +0530

    Update FoodItem.cs

--- models/FoodItem.cs
+++ models/FoodItem.cs
@@ -1,22 +1,20 @@
 using System;
-using System.Collections.Generic;
 
 namespace OnlineFoodOrderingSystem.Models
 {
     public class FoodItem
     {
         public int Id { get; set; }
-
         public string Name { get; set; } = "";
+
         public decimal Price { get; set; }
 
         public string Description { get; set; } = "";
-        public string ImageUrl { get; set; } = "";
-
-        public int RestaurantId { get; set; }
-        public Restaurant? Restaurant { get; set; }
 
         public int CategoryId { get; set; }
-        public Category? Category { get; set; }
+        public Category Category { get; set; }
+
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
     }
 }
