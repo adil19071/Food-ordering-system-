0efc693d633761b62445df77f75de0653ef46f50
adil19071 <blackheart12200@gmail.com>
2025-11-18 15:17:31 +0530

    Update Order.cs

--- models/Order.cs
+++ models/Order.cs
@@ -1,14 +1,14 @@
+using System;
+using System.Collections.Generic;
+
 namespace OnlineFoodOrderingSystem.Models
 {
     public class Order
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public string UserName { get; set; } = "";
-        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public DateTime OrderDate { get; set; }  // <-- THIS CAUSED ERROR
         public decimal TotalAmount { get; set; }
-        public string Status { get; set; } = "Pending";
 
-        public ICollection<OrderItem>? Items { get; set; }
+        public ICollection<OrderItem> Items { get; set; }
     }
 }
